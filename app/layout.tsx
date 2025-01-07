a433dbef8b4b05bc3f96c05769ef43d8606803aa
Faizan Haq <fizoneechan@gmail.com>
2025-01-07 12:02:42 +0500

    [refactor] Remove author metadata and add Open Graph and Twitter image meta tags in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -169,10 +169,6 @@ export const metadata = {
   keywords: ["Lumunate", "Portfolio", "Next.js", "React", "Web Development"],
   authors: [
     {
-      name: "Faizan Haq",
-      url: "https://fizoneechan.com"
-    },
-    {
       name: "Lumunate",
       url: "https://lumunate.com"
     },
@@ -189,6 +185,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <GoogleTagManager />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+        <meta name="twitter:image" content="<generated>" />
+        <meta name="twitter:image:type" content="<generated>" />
+        <meta name="twitter:image:width" content="<generated>" />
+        <meta name="twitter:image:height" content="<generated>" />
       </head>
       <body
         className={`${nacelle.variable} ${mendaFont.variable} ${ralewayFont.variable} bg-black font-raleway text-base text-gray-200 antialiased`}
